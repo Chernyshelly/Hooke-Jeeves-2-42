3f2c4732e41cb1fd49f3846cf0bb1ce2f3fe6adf
Chernyshelly <andreycherni@gmail.com>
2019-11-14 23:47:25 +0300

    Я сделал дичь для стабильных функций

--- Program.cs
+++ Program.cs
@@ -4,20 +4,57 @@ namespace Hooke_Jeeves
 {
     class Program
     {
-        static int Sq(int x)
+        static double Sq(double x)
         {
-            return x * x;
+            return (4+3*x);
         }
-        delegate int Func(int x);
-        static int User(int x, Func func)
+        delegate double Func(double x);
+        static double User(double leftX, double rightX, double startStep, double endStep, Func func)
         {
-            return func(x);
+            double step = startStep;
+            double x = 0;
+            double lastResult = func(x);
+            while(Math.Abs(step) > endStep)
+            {
+                x += step;
+                Console.WriteLine($"x={x} step={step} lr={lastResult} func={func(x)}");
+                if(x >= rightX)
+                {
+                    x -= step;
+                    step = -step;
+                }
+                else
+                {
+                    if(x > leftX)
+                    {
+                        if (func(x) > lastResult)
+                        {
+                            x -= step;
+                            step = -step;
+                            if (step > 0)
+                            {
+                                step = step / 2;
+                            }
+                        }
+                        else
+                        {
+                            lastResult = func(x);
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return x;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             //Func func = Sq;
-            Console.WriteLine(User(3, Sq));
+            Console.WriteLine(User(-100, 100, 10, 0.01, Sq));
         }
     }
 }
