589d2b5c78cd488baa4146cf6df3bac13086c855
Chernyshelly <andreycherni@gmail.com>
2019-11-14 21:19:14 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hooke_Jeeves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
