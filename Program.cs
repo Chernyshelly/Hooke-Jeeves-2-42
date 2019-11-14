1103cc838af9e37a6fd3cd73e5476bb634b6a3a4
Chernyshelly <andreycherni@gmail.com>
2019-11-14 22:00:23 +0300

    Delegate is cool

--- Program.cs
+++ Program.cs
@@ -4,9 +4,20 @@ namespace Hooke_Jeeves
 {
     class Program
     {
+        static int Sq(int x)
+        {
+            return x * x;
+        }
+        delegate int Func(int x);
+        static int User(int x, Func func)
+        {
+            return func(x);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Func func = Sq;
+            Console.WriteLine(User(3, Sq));
         }
     }
 }
