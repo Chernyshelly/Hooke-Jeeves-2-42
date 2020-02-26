0171a3645f8b7e748ddd31a3105a565bab645c01
Chernyshelly <andreycherni@gmail.com>
2020-02-27 00:32:50 +0300

    I think ready

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ namespace Hooke_Jeeves
 {
     class Program
     {
+        static bool AreAlmostEqual(double[] a, double[] b)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Math.Round(a[i], 4) != Math.Round(b[i], 4))
+                {
+                    return false;
+                }
+            }
+                
+            return true;
+        }
         static bool IsLesser(double[] a, double[] b)
         {
             for (int i = 0; i < a.Length; i++)
@@ -28,7 +40,7 @@ namespace Hooke_Jeeves
         }
         static double Sq(double x)
         {
-            return (Math.Cos(x));
+            return (x*x);
         }
         static double Tryy(double[] coords)
         {
@@ -48,18 +60,22 @@ namespace Hooke_Jeeves
         static double[] HookJeeves(double[] leftX, double[] rightX, double[] startCoords, double[] startStep, double[] endStep, Func func, int count)
         {
             bool quit = false;
+            bool skipFirstStage = false;
             double[] dot1 = startCoords;
             double[] dot2 = startCoords;
             double[] dot3;
             double[] dot4;
             double[] curStep = startStep;
-            while (quit == false)
+            while (true)
             {
-                while (IsLesser(endStep, curStep))
+                if (skipFirstStage == false)
                 {
-                    for (int i = 0; i < count; i++)
+                    while (IsLesser(endStep, curStep))
                     {
-                            double[] tempDot = dot1;
+                        for (int i = 0; i < count; i++)
+                        {
+                            double[] tempDot = new double[count];
+                            Array.Copy(dot1, tempDot, count);
                             tempDot[i] = dot1[i] + curStep[i];
                             double plusFunc = func(tempDot);
                             tempDot[i] = dot1[i] - curStep[i];
@@ -82,14 +98,21 @@ namespace Hooke_Jeeves
                                     }
                                 }
                             }
+                        }
+                    }
+                    if (quit == true)
+                    {
+                        return dot2;
                     }
                 }
 
                 dot3 = dot3Founder(dot1, dot2, 2);
-                dot4 = dot3;
+                dot4 = new double[count];
+                Array.Copy(dot3, dot4, count);
                 for (int i = 0; i < count; i++)
                 {
-                    double[] tempDot = dot3;
+                    double[] tempDot = new double[count];
+                    Array.Copy(dot1, tempDot, count);
                     tempDot[i] = dot3[i] + curStep[i];
                     double plusFunc = func(tempDot);
                     tempDot[i] = dot3[i] - curStep[i];
@@ -105,9 +128,24 @@ namespace Hooke_Jeeves
                             dot4[i] = dot4[i] - curStep[i];
                         }
                     }
+                    if (quit == true)
+                    {
+                        return dot4;
+                    }
                 }
+                    if (AreAlmostEqual(dot3, dot4))
+                    {
+                        Array.Copy(dot2, dot1, count);
+                        Array.Copy(dot4, dot2, count);
+                        skipFirstStage = true;
+                    }
+                    else
+                    {
+                        Array.Copy(dot2, dot1, count);
+                    }
+
+                quit = true;
             }
-            return coords;
         }
         static void Main(string[] args)
         {
